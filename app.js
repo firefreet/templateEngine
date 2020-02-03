8b5382eb392f503fd8fdfbde4819ff751c70aeb2
Jeremy Marotta <firefreet@gmail.com>
2020-02-02 21:30:26 -0500

    add function to read html template, create DOM & jquery

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const $ = require('jquery')
-const jsdom = require('jsdom')
+const { JSDOM } = require('jsdom')
+const { promisify } = require('util')
 const fs = require('fs')
 const Engineer = require('./lib/Engineer')
 const Intern = require('./lib/Intern')
@@ -7,26 +7,50 @@ const Manager = require('./lib/Manager')
 const questions = require('./lib/questions')
 const inquirer = require('inquirer')
 const personArray = []
+const readFilePromise = promisify(fs.readFile)
+var dom
+var $ = null;
+
+function createDOM(fileString) {
+    // create a Document Object Model type object
+    dom = new JSDOM(fileString);
+    // pass the DOM object into the jquery module and set as the normal $ variable
+    $ = require("jquery")(dom.window);
+    return [dom,$]
+}
 
 function createPerson(answers) {
-    const {name,email,id,role,school,officeNumber, gitName} = answers
+    const { name, email, id, role, school, officeNumber, gitName } = answers
     var newPerson
     switch (role) {
-        case 'Intern': newPerson = new Intern(name,id,email,school);
-        break;
-        case 'Manager': newPerson = new Manager(name,id,email,officeNumber)
-        break;
-        case 'Engineer': newPerson =  new Engineer(name,id,email,gitName)
-        break;
+        case 'Intern': newPerson = new Intern(name, id, email, school);
+            break;
+        case 'Manager': newPerson = new Manager(name, id, email, officeNumber)
+            break;
+        case 'Engineer': newPerson = new Engineer(name, id, email, gitName)
+            break;
     }
     personArray.push(newPerson)
-
-    return newPerson  
+    return newPerson
 }
+
 function getInfo() {
     inquirer
         .prompt(questions)
         .then(createPerson)
 }
+async function readFile() {
+    return await readFilePromise("./assets/template.html","utf8")
+        .then(createDOM)
+        
+}
+
+readFile()
 getInfo()
-module.exports = {createPerson, personArray}
+
+module.exports = {
+    createPerson,
+    personArray,
+    readFile,
+    readFilePromise
+}
--- tests/app.test.js
+++ tests/app.test.js
@@ -2,6 +2,7 @@ const app = require('../app')
 const Intern = require('../lib/Intern')
 const Manager = require('../lib/Manager')
 const Engineer = require('../lib/Engineer')
+const { JSDOM } = require('jsdom')
 
 
 const testAnswers = {
@@ -11,7 +12,9 @@ const testAnswers = {
     role: "Manager",
     school: "UCONN",
     officeNumber: "12",
-    gitName: "firefreet"
+    gitName: "firefreet",
+    anotherPerson: "No"
+
 }
 
 
@@ -36,10 +39,21 @@ test("When passed the Engineer role, is a Engineer object created with the git u
     expect(newPerson.getGithub()).toEqual(testAnswers.gitName)
 })
 
-test("When calling create Person, the new object is added to the personArray",() =>{
+test("When calling create Person, the new object is added to the personArray", () => {
     testAnswers.role = "Engineer"
     app.createPerson(testAnswers)
-    var index = app.personArray.length -1
+    var index = app.personArray.length - 1
     expect(app.personArray[index] !== null).toBe(true)
     expect(app.personArray[index] instanceof Engineer).toBe(true)
+})
+
+test("ReadFile creates a new JSDOM object and $ is assigned the jquery object", async () => {
+    var result = await app.readFile()
+    expect(result[0] instanceof JSDOM).toBe(true)
+    expect(result[1]["0"].hasOwnProperty("Window")).toBe(true)
+})
+
+test("readFilePromise returns a string", async () => {
+    var result = await app.readFilePromise("./assets/template.html","utf8")
+    expect(typeof result).toBe("string")
 })
